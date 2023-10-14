1c8ddb4862d3b553f094e54309b3da0a9d55655e
imvijayrao <raov2412@gmail.com>
2023-10-14 21:35:45 +0530

    initial

---
+++ demo/src/main/java/demo/Main.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Paintwalls question
+        int paintcost[] = {2,3,4,2};
+        int painttime[] = {1,1,1,1};
+        Painters p = new Painters();
+        int cost = p.paintwalls(paintcost, painttime);
+        System.out.println("The total cost is " + cost);
+    }
+}
